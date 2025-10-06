cf28cf1946f7b8e7fa9feefd3144d834ca20967e
anthonyA1214 <anthonyamiluddin1@gmail.com>
2025-10-07 00:55:25 +0800

    Implement a Root-Level Request Logger Middleware, Chain Middleware to Create a Time Server

--- myApp.js
+++ myApp.js
@@ -2,13 +2,18 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
-app.use('/public', express.static(__dirname +  "/public"));
-
 // console.log("Hello World");
 
 // app.get('/', (req, res) => {
 //     res.send('Hello Express');
 // });
+
+app.use('/public', express.static(__dirname +  "/public"));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} -  ${req.ip}`);
+    next();
+});
 
 app.get('/', (req, res) => {
     const path = __dirname + "/views/index.html";
@@ -26,6 +31,13 @@ app.get('/json', (req, res) => {
     }
 
     res.json(resObj);
-}); 
+});
+
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({"time": req.time});
+});
 
 module.exports = app;
